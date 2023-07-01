73dc3bd5f5fc99e1640b90bbc68ce65a366a717d
nicholasjamessmith <njsmith6@gmail.com>
2023-07-01 06:01:44 -0400

    full CRUD functionality, added number property value

--- models/animals.js
+++ models/animals.js
@@ -3,9 +3,10 @@ const mongoose = require('./connection')
 const animalSchema = new mongoose.Schema({
   name: { type: String, required: true },
   habitat: { type: String, required: true },
-  carnivore: Boolean
+  carnivore: Boolean,
+  lifeExpectancy: { type: Number, required: true }
 });
 
-const Book = mongoose.model(`animal`, animalSchema);
+const Animal = mongoose.model(`animal`, animalSchema);
 
 module.exports = Animal;
--- server.js
+++ server.js
@@ -1,18 +1,78 @@
 require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override')
 const Animal = require('./models/animals');
 const app = express();
 
 //Middleware
 app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride("_method"))
 
 //Routes
-app.get('/', (req, res) => {
+//Demo
+app.get("/", (req, res) => {
   res.send("Hello World!")
 })
 
+//INDUCES: update, edit,
 
+//Index
+app.get("/animals", async (req, res) => {
+  const allAnimals = await Animal.find({});
+  res.render("index.ejs", { animals: allAnimals })
+})
+
+//New
+app.get("/animals/new", (req, res) => {
+  res.render("new.ejs")
+})
+
+//Create
+app.post("/animals", async (req, res) => {
+  //fit your data into the model before 'create'ing
+  if (req.body.carnivore === 'on') {
+    req.body.carnivore = true;
+  } else {
+    req.body.carnivore = false;
+  }
+  //*life expectancy number - *parse int, or plus sign*
+  await Animal.create(req.body)
+  res.redirect("/animals")
+})
+
+//Show
+app.get("/animals/:id", async (req, res) => {
+  const idAnimal = await Animal.findById(req.params.id)
+  res.render("show.ejs", { animal: idAnimal })
+})
+
+//Destroy
+app.delete('/animals/:id', async (req, res) => {
+  await Animal.findByIdAndDelete(req.params.id)
+  res.redirect('/animals')
+})
+
+
+//Update Route *parse int, plus sign, or 3rd option*
+
+app.get('/animals/:id/edit', async (req, res) => {
+  const animal = await Animal.findById(req.params.id)
+  res.render('edit.ejs', { animal })
+})
+
+app.put('/animals/:id', async (req, res) => {
+  if (req.body.carnivore === 'on') {
+    req.body.carnivore = true;
+  } else {
+    req.body.carnivore = false;
+  }
+
+  await Animal.findByIdAndUpdate(req.params.id, req.body)
+
+  res.redirect('/animals');
+})
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
